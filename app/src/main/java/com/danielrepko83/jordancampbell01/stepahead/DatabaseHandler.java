26e099f7ec081135d42af07e0ae24223429f05f9
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2018-03-23 13:20:59 -0400

    #20 DatabaseHandler.java * Created a public static constant named 'DATABASE_VERSION' with a value of 1 * Created a public static constant named 'DATABASE_NAME' with the string 'stepahead' * Updated DatabaseHandler constructor to use the constants for the name and version of the database

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/DatabaseHandler.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/DatabaseHandler.java
@@ -9,9 +9,11 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DatabaseHandler extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "stepahead";
 
     public DatabaseHandler(Context context) {
-        super(context, null, null, 0);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     public void onCreate(SQLiteDatabase db) {
