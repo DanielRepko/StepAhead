8eeba77a81749d580859eb27e6e52fd358675376
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2018-03-23 13:46:42 -0400

    #19 Weight.java * Created private member variables for id, pounds, kilograms, and date * Created no-args constructor * Created constructor that takes pounds, kilograms, and date and sets the associated member variables * Created constructor that takes id, pounds, kilograms, and date and sets the associated member variables

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/Object_Classes/Weight.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/Object_Classes/Weight.java
@@ -5,5 +5,25 @@ package com.danielrepko83.jordancampbell01.stepahead.Object_Classes;
  */
 
 public class Weight {
-    
+    private int id;
+    private Double pounds;
+    private Double kilograms;
+    private String date;
+
+    public Weight() {
+
+    }
+
+    public Weight(Double pounds, Double kilograms, String date) {
+        this.pounds = pounds;
+        this.kilograms = kilograms;
+        this.date = date;
+    }
+
+    public Weight(int id, Double pounds, Double kilograms, String date) {
+        this.id = id;
+        this.pounds = pounds;
+        this.kilograms = kilograms;
+        this.date = date;
+    }
 }
