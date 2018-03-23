bbd2bb1fbc5c034bc7e70be4f2c591a9b1f8d6d1
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2018-03-23 13:18:15 -0400

    #20 DatabaseHandler.java * Implemented placeholder onCreate() method * Implemented placeholder onUpgrade() method * Implemented placeholder constructer

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/DatabaseHandler.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/DatabaseHandler.java
@@ -1,5 +1,7 @@
 package com.danielrepko83.jordancampbell01.stepahead;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
@@ -8,4 +10,15 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
+    public DatabaseHandler(Context context) {
+        super(context, null, null, 0);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
