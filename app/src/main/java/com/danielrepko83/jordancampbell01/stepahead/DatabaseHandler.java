be7e1593a4289a17c292a22990ce65958948712f
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2018-03-23 13:15:55 -0400

    #20 DatabaseHandler.java * Created DatabaseHandler class * Made DatabaseHandler extend SQLiteOpenHelper

---
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/DatabaseHandler.java
@@ -0,0 +1,11 @@
+package com.danielrepko83.jordancampbell01.stepahead;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by web on 2018-03-23.
+ */
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+}
