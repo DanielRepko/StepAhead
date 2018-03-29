7d1eb3776652b3f03c7be94c4121b9a5df149242
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2018-03-29 14:12:34 -0400

    #27 created private properties inside of the RunJournal class *made one property for each of the columns inside of the Run Table, except for teh images coloumn *realized that to allow the user to take multiple pictures and associate them with a single journal, that I will need to create a Pictures Table to hold the images *the RunJournal class may end up having an arraylist property to store all of a Journals images, or the association may be handled mostly in the database *will need to think about this matter further before moving forward

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/Object_Classes/RunJournal.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/Object_Classes/RunJournal.java
@@ -1,8 +1,24 @@
 package com.danielrepko83.jordancampbell01.stepahead.Object_Classes;
 
+import java.util.ArrayList;
+
 /**
  * Created by Daniel Repko on 2018-03-29.
  */
 
 public class RunJournal {
+
+    private int id;
+    private double distance;
+    private String startTime;
+    private int calories;
+    //private ArrayList<Integer> images;
+    private String feeling;
+    private String area;
+    private int heartRate;
+    private String note;
+    private double avgPace;
+    private double avgSpeed;
+    private String weather;
+    private int measurement;
 }
