b57cdf431657f662365c8995daf0c6d20cb33dda
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2018-04-08 15:47:08 -0400

    #49 created a Service called LocationTracker (created in earlier commit but forgot to list) *will be used to track location even when app is in the background *added some test code to help me figure out how flexible I can be with the Service

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/LocationTracker.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/LocationTracker.java
@@ -1,16 +1,40 @@
 package com.danielrepko83.jordancampbell01.stepahead;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.widget.TextView;
 
 public class LocationTracker extends Service {
+
+    private View view;
+
     public LocationTracker() {
+    }
+
+    public LocationTracker(View view){
+        this.view = view;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+
+    private void changeText(){
+        TextView distance = view.findViewById(R.id.distance);
+        distance.setText("it works");
+    }
+
+
 }
