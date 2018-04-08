3693a26e0cd8d5698ddfa754da30adff252e8ffb
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2018-04-08 15:18:27 -0400

    Issue #61 strings.xml * Added strings for the weight entry hint, and the weight add button fragment_weight.xml * Updated weightEditText's hint and submitButton's text to use string references WeightFragment.java * Separated the view from the return statement in onCreateView

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/WeightFragment.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/WeightFragment.java
@@ -66,7 +66,11 @@ public class WeightFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weight, container, false);
+        View view = inflater.inflate(R.layout.fragment_weight, container, false);
+
+        
+
+        return view;
     }
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
