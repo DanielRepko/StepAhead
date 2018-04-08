873629cf3f49ae76887ea8721bc0450539eab539
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2018-04-08 14:16:16 -0400

    #62 removed the runId property from the Picture class

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/Object_Classes/Picture.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/Object_Classes/Picture.java
@@ -11,7 +11,6 @@ public class Picture {
      */
 
     private int id;
-    private int runId;
     private String resource;
 
 
@@ -26,17 +25,14 @@ public class Picture {
     /**
      * The Picture class is used to hold image resources to be used with Run Journals
      * @param id the id of the picture in the database
-     * @param runId the id of the run journal the Picture is associated with
      * @param resource the resource code of the image
      */
-    public Picture(int id, int runId, String resource) {
+    public Picture(int id, String resource) {
         this.id = id;
-        this.runId = runId;
         this.resource = resource;
     }
 
-    public Picture(int runId, String resource) {
-        this.runId = runId;
+    public Picture(String resource) {
         this.resource = resource;
     }
 
@@ -50,14 +46,6 @@ public class Picture {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public int getRunId() {
-        return runId;
-    }
-
-    public void setRunId(int runId) {
-        this.runId = runId;
     }
 
     public String getResource() {
