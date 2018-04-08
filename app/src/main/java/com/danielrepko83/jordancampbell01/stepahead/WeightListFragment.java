f7aa5e667e04da9d6957b1c17f54eac8ef534ce2
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2018-04-08 16:37:59 -0400

    Issue #60, WeightListFragment.java * onCreateView, sepearated the view from the return statement * onCreateView, set the MainActivity's fab to replace the WeightListFragment with a WeightGraphFragment

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/WeightListFragment.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/WeightListFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +30,7 @@ public class WeightListFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    FragmentManager fm;
 
     public WeightListFragment() {
         // Required empty public constructor
@@ -64,7 +67,17 @@ public class WeightListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weight_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_weight_list, container, false);
+
+        MainActivity.fab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FragmentTransaction trans = fm.beginTransaction();
+                trans.replace(R.id.fragmentStorage, new WeightGraphFragment());
+                trans.commit();
+            }
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
