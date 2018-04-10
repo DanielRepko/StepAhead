e2e46937b24aa156e7225e42b89ccc0707a2e83a
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2018-04-10 12:33:33 -0400

    Issue #57, CustomAdapterWeight.java * Updated class to extend RecyclerView.Adapter, and implemented placeholder methods

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/CustomAdapterWeight.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/CustomAdapterWeight.java
@@ -1,5 +1,18 @@
 package com.danielrepko83.jordancampbell01.stepahead;
 
-public class CustomAdapterWeight {
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
+public class CustomAdapterWeight extends RecyclerView.Adapter {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    public int getItemCount() {
+        return 0;
+    }
 }
