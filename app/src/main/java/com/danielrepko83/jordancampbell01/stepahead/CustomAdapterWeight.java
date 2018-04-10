a8b957b1ca7cfc4b99110280e6d89d5f7889c7ff
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2018-04-10 12:37:39 -0400

    Issue #57, CustomAdapterWeight.java * Created a private ArrayList of Weights for storing the weight entries provided * Created a property to store the current context * Created a public constructor that takes and sets an ArrayList of Weight entries

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/CustomAdapterWeight.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/CustomAdapterWeight.java
@@ -1,9 +1,21 @@
 package com.danielrepko83.jordancampbell01.stepahead;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import com.danielrepko83.jordancampbell01.stepahead.Object_Classes.Weight;
+
+import java.util.ArrayList;
+
 public class CustomAdapterWeight extends RecyclerView.Adapter {
+    private ArrayList<Weight> weights;
+    Context context;
+
+    public CustomAdapterWeight(ArrayList<Weight> weights) {
+        this.weights = weights;
+    }
+
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
