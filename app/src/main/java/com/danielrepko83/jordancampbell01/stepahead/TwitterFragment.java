a33d7500f07d78227547df4b2f2cfad075f1d4a8
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2018-04-14 17:16:11 -0400

    Issue #80 strings.xml * Added the consumer key and secret to the strings TwitterFragment.java * Seperated view from the return statement, hid the fab button, and grabbed the twitterFeed recyclerview

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/TwitterFragment.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/TwitterFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -63,8 +64,13 @@ public class TwitterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_twitter, container, false);
+        View view = inflater.inflate(R.layout.fragment_twitter, container, false);
+        MainActivity.fab.hide();
+        RecyclerView twitterFeed = view.findViewById(R.id.twitterFeed);
+
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
