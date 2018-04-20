576dcdde1b9226e49cca226acb4c9e078b0081fd
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2018-04-20 13:33:05 -0400

    #129 added code to the submit button's onclickListener *when submit button is pressed, checks to see if value has been entered into the edit text *if not displays toast asking user to enter value *if so checks to see if one of the radio buttons have been selected and enters the weight based on the measurement selected *if neither of the radio buttons are selected displays a toast asking user to select a measurement

--- app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/CustomDialog.java
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/CustomDialog.java
@@ -8,9 +8,15 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.danielrepko83.jordancampbell01.stepahead.Object_Classes.Weight;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
- * Created by awsom on 4/20/2018.
+ * Created by Daniel Repko on 4/20/2018.
  */
 
 public class CustomDialog extends Dialog {
@@ -39,7 +45,36 @@ public class CustomDialog extends Dialog {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //check to see if a value has been entered
+                if(weight.getText().toString().equals("")){
+                    Toast.makeText(getContext(),
+                            "Please enter a weight.",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    //if kg is checked
+                    if(kg.isChecked()){
+                        DatabaseHandler db = new DatabaseHandler(getContext());
+                        Weight newWeight = new Weight(Double.parseDouble(weight.getText().toString()) * 2.2046, Calendar.getInstance().getTime()+"");
+                        db.addWeight(newWeight);
+                        db.close();
 
+                        //dismiss the dialog
+                        dismiss();
+
+                    } else if(lbs.isChecked()){
+                        DatabaseHandler db = new DatabaseHandler(getContext());
+                        Weight newWeight = new Weight(Double.parseDouble(weight.getText().toString()), Calendar.getInstance().getTime()+"");
+                        db.addWeight(newWeight);
+                        db.close();
+
+                        //dismiss the dialog
+                        dismiss();
+                    } else {
+                        Toast.makeText(getContext(),
+                                "Please select a weight measurement",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
