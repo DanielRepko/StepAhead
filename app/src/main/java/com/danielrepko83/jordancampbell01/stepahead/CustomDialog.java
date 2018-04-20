fe4c5d0d1a9619af44b5cf2d32b40c7570097e39
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2018-04-20 09:15:27 -0400

    #129 created a new class called CustomDialog *CustomDialog extends Dialog and implements View.OnClickListener *constructor takes an activity as its only parameter *CustomDialog will be used to ask the user for their weight on first app launch

---
+++ app/src/main/java/com/danielrepko83/jordancampbell01/stepahead/CustomDialog.java
@@ -0,0 +1,21 @@
+package com.danielrepko83.jordancampbell01.stepahead;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.View;
+
+/**
+ * Created by awsom on 4/20/2018.
+ */
+
+public class CustomDialog extends Dialog implements View.OnClickListener {
+
+    public CustomDialog(Activity activity){
+        super(activity);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
